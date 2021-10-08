db4c73d19f7ae2ceb0c79e86aa952da6ca3fd131
ch-4ml <ark9659@gmail.com>
2021-10-08 17:14:28 +0900

    update

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
-@ECHO OFF
+@ECHO
 
 CALL stop.bat
 
-./studylab-api.exe
-npx serve -l 3000 -s .
+START ./studylab-api.exe
+
+START npx serve -l 3000 -s .
--- stop.bat
+++ stop.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
+@ECHO
 
 SET FRONT_PORT=3000
 
@@ -7,5 +7,3 @@ SET BACK_PORT=1114
 for /f "tokens=5" %%p in ('netstat -aon ^| find /i "listening" ^| find "%FRONT_PORT%"') do taskkill /F /PID %%p
 
 for /f "tokens=5" %%p in ('netstat -aon ^| find /i "listening" ^| find "%BACK_PORT%"') do taskkill /F /PID %%p
-
-pause
