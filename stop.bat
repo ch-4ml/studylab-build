1bf0992d92d28ca75367480e6847be2d32f50c7d
ch-4ml <fetch@ajou.ac.kr>
2022-01-09 14:28:48 +0900

    update files

--- stop.bat
+++ stop.bat
@@ -2,7 +2,7 @@
 
 SET FRONT_PORT=PARAM_CLIENT_PORT
 
-SET BACK_PORT=1116[1]
+SET BACK_PORT=1116[2]
 
 for /f "tokens=5" %%p in ('netstat -aon ^| find /i "listening" ^| find "%FRONT_PORT%"') do taskkill /F /PID %%p
 
